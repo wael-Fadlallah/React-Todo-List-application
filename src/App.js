cdba1bdeca0504908a059cb3614074b00ef6c9bb
wael-Fadlallah <wael.fudlallah@gmail.com>
2021-06-05 13:03:54 +0400

    delete item

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import TodoPanel from "./components/TodoPanel";
-import todoList from "./todoList";
+// import todoList from "./todoList";
 import React from "react";
 import base from "./base";
+import { isEmpty } from "lodash";
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      todos: todoList,
+      todos: [],
     };
   }
 
@@ -19,35 +20,78 @@ class App extends React.Component {
       state: "todos",
     });
   }
+
   render() {
     let lastCheckedIndex = null;
 
     const checkItem = (index, event) => {
-      if (event.shiftKey !== undefined) {
-        console.log(event.shiftKey);
-        // take a copy
-        const todo = [...this.state.todos];
+      // below was for hold shift and change
+      // if (event.shiftKey !== undefined) {
+      // take a copy
+      const todos = [...this.state.todos];
 
-        todo[index] = { ...todo[index], complete: !todo[index].complete };
+      todos[index] = { ...todos[index], complete: !todos[index].complete };
 
-        this.setState({ todos: todo });
+      this.setState({ todos });
 
-        lastCheckedIndex = index;
-      }
+      lastCheckedIndex = index;
+      // }
     };
-    const Todo = this.state.todos.map((todo, index) => (
-      <TodoPanel
-        todo={todo.todo}
-        checked={todo.complete}
-        key={index}
-        index={index}
-        checkItem={checkItem}
-      />
-    ));
+    const loadTodo = () => {
+      if (this.state.todos.length != undefined)
+        return this.state.todos.map((todo, index) => (
+          <TodoPanel
+            todo={todo.todo}
+            checked={todo.complete}
+            key={index}
+            index={index}
+            checkItem={checkItem}
+            deleteItem={deleteItem}
+          />
+        ));
+    };
+
+    const addTodo = (e) => {
+      e.preventDefault();
+      // create a new element
+      const todo = { todo: e.target.elements.todo.value, complete: false };
+
+      // take a copy of the state
+      if (isEmpty(this.state.todos)) {
+        var todos = [];
+      } else {
+        var todos = [...this.state.todos];
+      }
+      todos.push(todo);
+      this.setState({ todos });
+      e.target.reset();
+    };
+
+    const deleteItem = (index) => {
+      // take a copy
+      const todos = [...this.state.todos];
+      delete todos[index];
+      this.setState({ todos });
+    };
 
     return (
       <div className="App">
-        <div className="inbox">{Todo}</div>
+        <h1> To-do list </h1>
+        <div className="inbox">
+          {loadTodo()}
+
+          <div className="add-todo">
+            <form onSubmit={(e) => addTodo(e)}>
+              <input
+                type="text"
+                placeholder="Add todo item"
+                name="todo"
+                style={{ height: "40px", width: "80%", border: "none" }}
+              />
+              <button style={{ height: "40px", width: "18%" }}>📓 Add</button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/TodoPanel.js
+++ src/components/TodoPanel.js
@@ -4,13 +4,20 @@ class TodoPanel extends React.Component {
   render() {
     return (
       <div className="item">
-        <input
-          type="checkbox"
-          checked={this.props.checked}
+        <span
+          id="span"
           onClick={(e) => this.props.checkItem(this.props.index, e)}
-          onChange={(e) => this.props.checkItem(this.props.index, e)}
-        />
+        >
+          <input
+            type="checkbox"
+            checked={this.props.checked}
+            onChange={(e) => this.props.checkItem(this.props.index, e)}
+          />
+        </span>
         <p>{this.props.todo}</p>
+        <button onClick={(e) => this.props.deleteItem(this.props.index)}>
+          ❌{" "}
+        </button>
       </div>
     );
   }
